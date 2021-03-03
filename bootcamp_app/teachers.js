1a85d8c8f2777bb5f96ca1f30820a96ae5cfe5c5
yozdil <yamac.ozdil@outlook.com>
2021-03-03 19:16:33 +0000

    Node Postgres

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,6 +8,7 @@ const pool = new Pool({
 });
 const argv = process.argv.slice(2);
 const cohortName = argv[0];
+console.log(cohortName);
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort 
@@ -15,11 +16,10 @@ FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${cohortName || 'JUL02'}%'
+WHERE cohorts.name LIKE '%${cohortName.toUpperCase() || 'JUL02'}%'
 ORDER BY teachers.name
 `)
 .then(res => {
-  // console.log(res.rows);
   res.rows.forEach((row) => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
